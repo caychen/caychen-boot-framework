717435f911cb523b9382ad9a1e736215adacebd8
caychen <412425870@qq.com>
2021-08-25 14:58:03 +0800

    去除依赖

--- caychen-boot-common/src/main/java/com/caychen/common/utils/EnumUtils.java
+++ caychen-boot-common/src/main/java/com/caychen/common/utils/EnumUtils.java
@@ -1,17 +1,12 @@
 package com.caychen.common.utils;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.stream.Stream;
 
 /**
- * <p>
- *
- * @author Jerry
- * @version 1.0
- * @since 2020/11/5
+ * @Author: Caychen
+ * @Date: 2021/8/25 11:25
+ * @Description:
  */
-@Slf4j
 public abstract class EnumUtils {
 
     public static <T> T fromCode(Class<T> enumClass, int code) {
@@ -21,7 +16,7 @@ public abstract class EnumUtils {
                     try {
                         enumCode = (int) enumClass.getMethod("getCode").invoke(enumObj);
                     } catch (Exception e) {
-                        log.error("无法读取code值", e);
+                        e.printStackTrace();
                     }
                     return enumCode == code;
                 })
