27beb423b5909151addfb7d399908aa43cbba54f
caychen <412425870@qq.com>
2021-08-25 14:59:03 +0800

    常量定义

---
+++ caychen-boot-common/src/main/java/com/caychen/common/constant/HttpConstant.java
@@ -0,0 +1,15 @@
+package com.caychen.common.constant;
+
+/**
+ * @Author: Caychen
+ * @Date: 2021/8/25 14:25
+ * @Description:
+ */
+public class HttpConstant {
+
+    public static final String FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    public static final String HTTP = "http";
+
+    public static final String HTTPS = "https";
+}
