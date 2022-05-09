1c71e533f5c93990ed8fcbb8a1483889f3ab9749
caychen <412425870@qq.com>
2022-05-09 16:56:23 +0800

    添加admin依赖&测试

---
+++ caychen-boot-admin-server/src/main/java/com/caychen/boot/admin/server/AdminServerConfig.java
@@ -0,0 +1,15 @@
+package com.caychen.boot.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Caychen
+ * @Date: 2022/5/9 15:09
+ * @Description:
+ */
+@Configuration
+@EnableAdminServer
+public class AdminServerConfig {
+
+}
