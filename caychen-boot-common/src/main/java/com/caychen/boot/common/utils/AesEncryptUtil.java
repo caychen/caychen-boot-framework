56aafb90c3a924a0141d9567ad49367a6632047d
caychen <412425870@qq.com>
2023-03-20 20:47:58 +0800

    aes加密

--- caychen-boot-common/src/main/java/com/caychen/boot/common/utils/AesEncryptUtil.java
+++ caychen-boot-common/src/main/java/com/caychen/boot/common/utils/AesEncryptUtil.java
@@ -1,9 +1,11 @@
 package com.caychen.boot.common.utils;
 
 import com.caychen.boot.common.constant.CommonConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -11,13 +13,14 @@ import javax.crypto.spec.SecretKeySpec;
  * @Date: 2021/8/26 19:52
  * @Description:
  */
+@Slf4j
 public class AesEncryptUtil {
 
     //秘钥
-    private static final byte[] raw = Base64.decodeBase64(CommonConstant.ORIGIN_ENCRYPT_STR);
+    private static final byte[] DEFAULT_RAW_KEY = Base64.decodeBase64(CommonConstant.ORIGIN_ENCRYPT_STR);
 
-    public static String Encrypt(String sSrc) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+    public static String aesEncrypt(String sSrc) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(DEFAULT_RAW_KEY, "AES");
         //"算法/模式/补码方式"
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
@@ -26,23 +29,58 @@ public class AesEncryptUtil {
         return new Base64().encodeToString(encrypted);
     }
 
-    public static String Decrypt(String sSrc) throws Exception {
+    public static String aesDecrypt(String sSrc) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(DEFAULT_RAW_KEY, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        //先用base64解密
+        byte[] encrypted = new Base64().decode(sSrc);
         try {
-            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-            //先用base64解密
-            byte[] encrypted1 = new Base64().decode(sSrc);
-            try {
-                byte[] original = cipher.doFinal(encrypted1);
-                return new String(original, "utf-8");
-            } catch (Exception e) {
-                System.out.println(e.toString());
-                return null;
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
+            byte[] original = cipher.doFinal(encrypted);
+            return new String(original, "utf-8");
+        } catch (Exception e) {
+            log.error("AES解密失败：", e);
             return null;
         }
+
     }
+
+    public static String aesEncrypt(String data, String key, String iv) throws Exception {
+        if (StringUtils.isBlank(data) || StringUtils.isBlank(key)) {
+            return null;
+        }
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(), "AES"), new IvParameterSpec(iv.getBytes()));
+        byte[] bytes = cipher.doFinal(data.getBytes());
+
+        // Base64加密
+        return new Base64().encodeToString(bytes);
+    }
+
+    /**
+     * AES/CBC/PKCS5Padding 解密
+     *
+     * @param data 待解密内容(Base64加密串)
+     * @param key     密钥
+     * @param iv      向量
+     * @return 解密后的值
+     */
+    public static String aesDecrypt(String data, String key, String iv) throws Exception {
+        if (StringUtils.isBlank(data) || StringUtils.isBlank(key)) {
+            return null;
+        }
+
+        byte[] encrypted = new Base64().decode(data);
+
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
+
+
+        byte[] decData = cipher.doFinal(encrypted);
+
+        return new String(decData, "utf-8");
+    }
+
 }
--- caychen-boot-common/src/test/java/com/caychen/boot/common/AesEncryptUtilTest.java
+++ caychen-boot-common/src/test/java/com/caychen/boot/common/AesEncryptUtilTest.java
@@ -13,14 +13,24 @@ public class AesEncryptUtilTest {
     @Test
     public void test() throws Exception {
         // 需要加密的字串
-        String cSrc = "test_data";
+        String cSrc = "今天是周二，我好开心";
         System.out.println(cSrc);
         // 加密
-        String enString = AesEncryptUtil.Encrypt(cSrc);
+        String enString = AesEncryptUtil.aesEncrypt(cSrc);
         System.out.println("加密后的字串是：" + enString);
 
         // 解密
-        String DeString = AesEncryptUtil.Decrypt(enString);
+        String DeString = AesEncryptUtil.aesDecrypt(enString);
         System.out.println("解密后的字串是：" + DeString);
+
+        String key = "zhaoyanjunzhaoy1";
+
+        String iv = "1234567890123456";
+
+        String s = AesEncryptUtil.aesEncrypt(cSrc, key, iv);
+        System.out.println(s);
+
+        String s1 = AesEncryptUtil.aesDecrypt(s, key, iv);
+        System.out.println(s1);
     }
 }
